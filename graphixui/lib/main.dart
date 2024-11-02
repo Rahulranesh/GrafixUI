9d8ebb3fa83faa14965be7ef413c4236cc11633f
Srikasun <sri07072005@gmail.com>
2024-11-02 09:23:16 +0530

    commit

--- graphixui/lib/main.dart
+++ graphixui/lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:graphixui/pages/login_page.dart';
+import 'package:graphixui/pages/register_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(onTap: () {}),
+      home: LoginPage(),
     );
   }
 }
--- graphixui/lib/pages/login_page.dart
+++ graphixui/lib/pages/login_page.dart
@@ -1,103 +1,138 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:graphixui/components/my_button.dart';
 import 'package:graphixui/components/my_textfield.dart';
 
 class LoginPage extends StatefulWidget {
-  final void Function()? onTap;
-
-  LoginPage({super.key, required this.onTap});
-
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController emailController = TextEditingController();
-
+  bool showPassword = false;
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  //log-in
+  void togglePasswordVisibility() {
+    setState(() {
+      showPassword = !showPassword;
+    });
+  }
+
+  void onLogin() {
+    if (usernameController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty) {
+      // Implement your login logic here
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Please fill all the fields")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //logo
-          Icon(
-            Icons.lock_open_rounded,
-            size: 100,
-            color: Colors.grey.shade400,
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Text('Welcome Back ! You"ve been missed !'),
-          SizedBox(
-            height: 25,
-          ),
-
-          //welcome msg
-          Text(
-            'TicketVerse',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey.shade700,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        // Adjust height as needed
+        backgroundColor: const Color.fromARGB(
+            255, 8, 5, 61), // Transparent to show the image only
+        flexibleSpace: Container(
+          padding: EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logo.png'), // Your logo path here
+              // Fill the header area
             ),
           ),
-          SizedBox(
-            height: 25,
-          ),
-
-          //email
-          MyTextField(
-              controller: emailController,
-              hintText: 'Enter the email',
-              obscureText: false),
-          SizedBox(
-            height: 10,
-          ),
-
-          //password
-          MyTextField(
-              controller: passwordController,
-              hintText: 'Enter the password',
-              obscureText: true),
-          SizedBox(
-            height: 15,
-          ),
-
-          //sign-in
-          MyButton(onTap: () => () {}, text: 'Sign-In'),
-
-          SizedBox(
-            height: 25,
-          ),
-
-          //register
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Not a member ? ',
-                style: TextStyle(
-                  color: Colors.grey.shade600,
-                ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Login ',
+              style: GoogleFonts.roboto(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
-              GestureDetector(
-                onTap: widget.onTap,
-                child: Text(
-                  'Register now ',
-                  style: TextStyle(
-                      color: Colors.grey.shade700, fontWeight: FontWeight.bold),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MyTextField(
+                      controller: usernameController,
+                      hintText: "Username",
+                      obscureText: false),
+                  SizedBox(height: 16),
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      obscureText: true),
+                  SizedBox(height: 20),
+                  Center(child: MyButton(onTap: () {}, text: "Login")),
+                  SizedBox(height: 20),
+                  Center(
+                    child: Column(
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            // Google login handler
+                          },
+                          icon: Icon(Icons.login,
+                              color:
+                                  Colors.white), // Placeholder for Google icon
+                          label: Text('Login with Google'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            // Facebook login handler
+                          },
+                          icon: Icon(Icons.facebook,
+                              color: Colors
+                                  .white), // Placeholder for Facebook icon
+                          label: Text('Login with Facebook'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue.shade900,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        // Navigate to the registration screen
+                      },
+                      child: Text(
+                        'New User? Sign Up',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
