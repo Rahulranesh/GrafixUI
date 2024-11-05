8cb17633d9fe989631f100270592fab94b4450a8
Srikasun <sri07072005@gmail.com>
2024-11-05 14:59:19 +0530

    second commit

--- graphixui/lib/pages/register_page.dart
+++ graphixui/lib/pages/register_page.dart
@@ -3,105 +3,159 @@ import 'package:graphixui/components/my_button.dart';
 import 'package:graphixui/components/my_textfield.dart';
 
 class RegisterPage extends StatefulWidget {
-  final void Function()? onTap;
-  RegisterPage({super.key, required this.onTap});
-
   @override
-  State<RegisterPage> createState() => _RegisterPageState();
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
+  String selectedRole = 'User';
+  bool checkbox = false;
+  bool showPassword = false;
 
-  final TextEditingController confirmPasswordController =
-      TextEditingController();
+  void togglePasswordVisibility() {
+    setState(() {
+      showPassword = !showPassword;
+    });
+  }
+
+  void onSubmit() {
+    if (!checkbox) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: Text("Please accept the terms and conditions"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      );
+    } else if (firstNameController.text.isEmpty ||
+        lastNameController.text.isEmpty ||
+        usernameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: Text("Please fill all the fields"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      );
+    } else {
+      // Handle registration logic
+      print("Registering...");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //logo
-          Icon(
-            Icons.lock_open_rounded,
-            size: 100,
-            color: Theme.of(context).colorScheme.inversePrimary,
-          ),
-          SizedBox(
-            height: 25,
-          ),
-
-          //welcome msg
-          Text(
-            'Lets create an account for you',
-            style: TextStyle(
-              fontSize: 16,
-              color: Theme.of(context).colorScheme.inversePrimary,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 8, 5, 61),
+        flexibleSpace: Container(
+          padding: EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logo.png'),
             ),
           ),
-          SizedBox(
-            height: 25,
-          ),
-
-          //email
-          MyTextField(
-              controller: emailController,
-              hintText: 'Enter the email',
-              obscureText: false),
-          SizedBox(
-            height: 10,
-          ),
-
-          //password
-          MyTextField(
-              controller: passwordController,
-              hintText: 'Enter the password',
-              obscureText: true),
-          SizedBox(
-            height: 10,
-          ),
-          //confirmPassword
-          MyTextField(
-              controller: confirmPasswordController,
-              hintText: 'Confirm password',
-              obscureText: true),
-          SizedBox(
-            height: 15,
-          ),
-
-          //sign-up
-          MyButton(onTap: () => () {}, text: 'Sign-Up'),
-
-          SizedBox(
-            height: 25,
-          ),
-
-          //register
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Already have an account ?',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.inversePrimary,
-                ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Wrap the Column in a SingleChildScrollView
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Create an Account',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
+            Text('Make your events visible by ticketverse',
+                style: TextStyle(color: Colors.grey)),
+            SizedBox(height: 16),
+            DropdownButtonFormField<String>(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              borderRadius: BorderRadius.circular(18),
+              value: selectedRole,
+              items: <String>['User', 'Organizer', 'Admin'].map((String role) {
+                return DropdownMenuItem<String>(
+                  value: role,
+                  child: Text(role),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedRole = newValue!;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Select Role',
+                border: OutlineInputBorder(),
               ),
-              GestureDetector(
-                onTap: widget.onTap,
-                child: Text(
-                  'Login now ',
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            MyTextField(
+                controller: firstNameController,
+                hintText: "Firstname",
+                obscureText: false),
+            SizedBox(height: 16),
+            MyTextField(
+                controller: lastNameController,
+                hintText: "Lastname",
+                obscureText: false),
+            SizedBox(height: 16),
+            MyTextField(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false),
+            SizedBox(height: 16),
+            MyTextField(
+                controller: emailController,
+                hintText: "Email",
+                obscureText: false),
+            SizedBox(height: 16),
+            MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: !showPassword), // Show password conditionally
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Checkbox(
+                  value: checkbox,
+                  onChanged: (value) {
+                    setState(() {
+                      checkbox = value!;
+                    });
+                  },
                 ),
-              ),
-            ],
-          )
-        ],
+                Expanded(
+                  child: Text('I agree to terms & Policy.'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            MyButton(
+                onTap: onSubmit,
+                text:
+                    "Register") // Ensure the button calls the onSubmit function
+          ],
+        ),
       ),
     );
   }
